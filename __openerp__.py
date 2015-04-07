cf2b901d7edde98b6f5f2fda51cde1e06414f2bc
Martial MBE <a.mbe@cloudfacile.com>
2015-04-07 20:49:16 +0100

    Création de fichiers

---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    CloudFacile SYSCOHADA on OpenERP, Open Source Management Solution
+#    Copyright (C) 2015  (<http://cloudfacile.com>).
+#
+##############################################################################
+
+{
+    'name' : 'CAMEROON OHADA - Accounting',
+    'version' : '1.0',
+    'author' : 'Martial MBE (CloudFacile)',
+    'category' : 'Localization/Account Charts',
+    'description': """
+This module implements the accounting chart for Cameroon OHADA area.
+===========================================================
+    """,
+    'depends' : ['account','l10n_syscohada'],
+    'demo' : [],
+    'data' : ['cf_l10n_syscohada_data.xml'],
+    'installable': True,
+    'auto_install': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
